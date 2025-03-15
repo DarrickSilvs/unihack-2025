c2b446dc8ce25721eced3e27ad6e5569b1e83065
kennethgo <gennethko@gmail.com>
2025-03-15 21:22:06 +1100

    Removed secret

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -12,7 +12,7 @@ const center = {
 
 function Map({ location }) {
     const { isLoaded, loadError } = useLoadScript({
-        googleMapsApiKey: "AIzaSyAfLIy-cY6MG9tfX_dwkR9L7KMHnEMI6wo",
+        googleMapsApiKey: "",
     });
 
     if (loadError) return <p>Error loading maps</p>;

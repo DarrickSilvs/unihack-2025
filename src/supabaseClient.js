7a5433a12278e0b392e0ae5c6de6c5ffeeda780a
kennethgo <gennethko@gmail.com>
2025-03-15 13:04:09 +1100

    Reset

--- src/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabase_url = process.env.SUPABASE_URL;
-const supabase_key = process.env.SUPABASE_ANON_KEY;
-
-const supabase = createClient(supabase_url, supabase_key);
-export default supabase;
